ed49ec74d46c1ac229a3d1bbebaa16130d367ee1
Ted Shaffer <ted@brightsign.biz>
2020-08-30 07:04:30 -0700

    resize - attempt one. not what I need.

--- src/component/Template.tsx
+++ src/component/Template.tsx
@@ -1,12 +1,12 @@
 import * as React from 'react';
-import { useState } from 'react';
+// import { useState } from 'react';
 import { connect } from 'react-redux';
 import { style } from 'typestyle';
 import * as csstips from 'csstips';
 import clsx from 'clsx';
 
-import Button from '@material-ui/core/Button';
-import TextField from '@material-ui/core/TextField';
+// import Button from '@material-ui/core/Button';
+// import TextField from '@material-ui/core/TextField';
 
 // -----------------------------------------------------------------------
 // Types
@@ -22,17 +22,33 @@ export interface TemplateProps {
 // -----------------------------------------------------------------------
 
 const containerStyle = style(csstips.fillParent, csstips.vertical);
-const headerContainerStyle = () => style(
-  {
-    backgroundColor: 'white',
-    color: 'black',
-  },
-  csstips.centerCenter,
-  csstips.padding(10, 0, 35, 0),
-  csstips.content,
-  csstips.vertical,
-  csstips.verticallySpaced(10)
-);
+// const headerContainerStyle = () => style(
+//   {
+//     backgroundColor: 'white',
+//     color: 'black',
+//   },
+//   csstips.centerCenter,
+//   csstips.padding(10, 0, 35, 0),
+//   csstips.content,
+//   csstips.vertical,
+//   csstips.verticallySpaced(10)
+// );
+
+/*
+<html>
+<head></head>
+ <body>
+    <div style="width: 100%;">
+        <div style="width: 50%; height: 100px; float: left; background: green;">
+            Left Div
+        </div>
+        <div style="margin-left: 50%; height: 100px; background: blue;">
+            Right Div
+        </div>
+    </div>
+ </body>
+</html>
+*/
 
 // -----------------------------------------------------------------------
 // Component
@@ -54,63 +70,93 @@ const useStyles = makeStyles((theme) => ({
   textField: {
     width: '25ch',
   },
+  divContainers: {
+    resize: 'both',
+    overflow: 'scroll'
+  },
+  leftDiv: {
+    width: '40%',
+    height: '600px',
+    float: 'left',
+    background: 'green',
+    resize: 'both',
+    overflow: 'scroll',
+    border: '1px solid black'
+  },
+  rightDiv: {
+    marginLeft: '50%',
+    height: '600px',
+    background: 'blue',
+    resize: 'both',
+    overflow: 'scroll',
+    border: '1px solid black'
+  }
 }));
 
 const TemplateComponent = () => {
 
+  // const [pizzaState, setPizza] = useState('pizzaFace');
+
   const classes = useStyles();
 
-  const [pizzaState, setPizza] = useState('pizzaFace');
+  // const handleEatPizza = (e: any) => {
+  //   // tslint:disable-next-line: no-console
+  //   console.log('handleEatPizza');
+  // };
 
-  const handleEatPizza = (e: any) => {
-    // tslint:disable-next-line: no-console
-    console.log('handleEatPizza');
-  };
+  // const handlePizzaNameChange = (e: any) => {
+  //   // tslint:disable-next-line: no-console
+  //   console.log('setPizza: ', e.target.value);
+  //   setPizza(e.target.value);
+  // };
 
-  const handlePizzaNameChange = (e: any) => {
-    // tslint:disable-next-line: no-console
-    console.log('setPizza: ', e.target.value);
-    setPizza(e.target.value);
-  };
+  // const onFormSubmit = (e: any) => {
+  //   e.preventDefault();
+  //   handleEatPizza(e);
+  // };
 
-  const onFormSubmit = (e: any) => {
-    e.preventDefault();
-    handleEatPizza(e);
-  };
-
-  const renderHeader = () => {
+  const renderDivs = () => {
     return (
-      <div className={headerContainerStyle()}>
-        <h1># BrightSign</h1>
-        <p>UI Template Project</p>
-        <p>{pizzaState}</p>
-        <form noValidate={true} autoComplete='off' onSubmit={onFormSubmit}>
-          <div>
-            <TextField
-              required={true}
-              id='standard-required'
-              label='Pizza name'
-              className={clsx(classes.margin, classes.textField)}
-              onChange={handlePizzaNameChange}
-            />
-          </div>
-          <div>
-            <Button
-              type='submit'
-              variant='contained'
-              className={clsx(classes.margin)}
-            >
-              Eat Pizza
-            </Button>
-          </div>
-        </form>
+      <div className={clsx(classes.divContainers)}>
+        <div className={clsx(classes.leftDiv)}>Left Div</div>
+        <div className={clsx(classes.rightDiv)}>Right Div</div>
       </div>
     );
   };
 
+  // const renderHeader = () => {
+  //   return (
+  //     <div className={headerContainerStyle()}>
+  //       <h1># BrightSign</h1>
+  //       <p>UI Template Project</p>
+  //       <p>{pizzaState}</p>
+  //       <form noValidate={true} autoComplete='off' onSubmit={onFormSubmit}>
+  //         <div>
+  //           <TextField
+  //             required={true}
+  //             id='standard-required'
+  //             label='Pizza name'
+  //             className={clsx(classes.margin, classes.textField)}
+  //             onChange={handlePizzaNameChange}
+  //           />
+  //         </div>
+  //         <div>
+  //           <Button
+  //             type='submit'
+  //             variant='contained'
+  //             className={clsx(classes.margin)}
+  //           >
+  //             Eat Pizza
+  //           </Button>
+  //         </div>
+  //       </form>
+  //     </div>
+  //   );
+  // };
+
   return (
     <div className={containerStyle}>
-      {renderHeader()}
+      {renderDivs()}
     </div>
   );
 };
