b6b5e02036b04d3f2cab2c600134e65107ed8548
Ted Shaffer <ted@brightsign.biz>
2020-08-31 07:15:47 -0700

    playing with material ui

--- src/component/App.tsx
+++ src/component/App.tsx
@@ -3,8 +3,14 @@ import { useEffect, useState } from 'react';
 import { connect } from 'react-redux';
 import { loadLog } from '../controllers';
 import { bindActionCreators } from 'redux';
+import clsx from 'clsx';
+
 import { getHsmEvents } from '../selectors/events';
 import { HsmEvent } from '../type';
+
+import { makeStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
+import List from '@material-ui/core/List';
 
 // -----------------------------------------------------------------------
 // Types
@@ -18,12 +24,37 @@ import { HsmEvent } from '../type';
 // Component
 // -----------------------------------------------------------------------
 
+const useStyles = makeStyles((theme) => ({
+  margin: {
+    margin: theme.spacing(1),
+  },
+  input: {
+    width: 42,
+  },
+  quarterWidth: {
+    width: '25%',
+  },
+  formControl: {
+    margin: theme.spacing(1),
+    minWidth: 180,
+    display: 'inline',
+  },
+  inlineDiv: {
+    display: 'inline',
+  },
+  selectControl: {
+    minWidth: 96,
+  }
+}));
+
 interface AppProps {
   events: HsmEvent[];
   onLoadLogs: () => any;
 }
 
 const AppComponent = (props: AppProps | any) => {
+
+  const classes = useStyles();
 
   const [_logLoaded, setLogLoaded] = useState(false);
 
@@ -36,9 +67,17 @@ const AppComponent = (props: AppProps | any) => {
 
   console.log(props.events.length);
 
+  // https://stackoverflow.com/questions/46044589/dynamically-resize-columns-in-css-grid-layout-with-mouse
+  // https://v4-9-14.material-ui.com/api/list/
   return (
     <div>
-      Pizza
+      <Button
+        type='submit'
+        variant='contained'
+        className={clsx(classes.margin)}
+      >
+        Add Review
+      </Button>
     </div>
   );
 };
