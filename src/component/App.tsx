f16ab8d10a0a357e1efcb99ba95b717236bcd4cd
Ted Shaffer <ted@brightsign.biz>
2020-08-31 07:21:50 -0700

    initial use of material ui lists

--- src/component/App.tsx
+++ src/component/App.tsx
@@ -3,14 +3,15 @@ import { useEffect, useState } from 'react';
 import { connect } from 'react-redux';
 import { loadLog } from '../controllers';
 import { bindActionCreators } from 'redux';
-import clsx from 'clsx';
+// import clsx from 'clsx';
 
 import { getHsmEvents } from '../selectors/events';
 import { HsmEvent } from '../type';
 
-import { makeStyles } from '@material-ui/core/styles';
-import Button from '@material-ui/core/Button';
+// import { makeStyles } from '@material-ui/core/styles';
+// import Button from '@material-ui/core/Button';
 import List from '@material-ui/core/List';
+import { ListItemText, ListItem } from '@material-ui/core';
 
 // -----------------------------------------------------------------------
 // Types
@@ -24,28 +25,28 @@ import List from '@material-ui/core/List';
 // Component
 // -----------------------------------------------------------------------
 
-const useStyles = makeStyles((theme) => ({
-  margin: {
-    margin: theme.spacing(1),
-  },
-  input: {
-    width: 42,
-  },
-  quarterWidth: {
-    width: '25%',
-  },
-  formControl: {
-    margin: theme.spacing(1),
-    minWidth: 180,
-    display: 'inline',
-  },
-  inlineDiv: {
-    display: 'inline',
-  },
-  selectControl: {
-    minWidth: 96,
-  }
-}));
+// const useStyles = makeStyles((theme) => ({
+//   margin: {
+//     margin: theme.spacing(1),
+//   },
+//   input: {
+//     width: 42,
+//   },
+//   quarterWidth: {
+//     width: '25%',
+//   },
+//   formControl: {
+//     margin: theme.spacing(1),
+//     minWidth: 180,
+//     display: 'inline',
+//   },
+//   inlineDiv: {
+//     display: 'inline',
+//   },
+//   selectControl: {
+//     minWidth: 96,
+//   }
+// }));
 
 interface AppProps {
   events: HsmEvent[];
@@ -54,7 +55,7 @@ interface AppProps {
 
 const AppComponent = (props: AppProps | any) => {
 
-  const classes = useStyles();
+  // const classes = useStyles();
 
   const [_logLoaded, setLogLoaded] = useState(false);
 
@@ -71,13 +72,14 @@ const AppComponent = (props: AppProps | any) => {
   // https://v4-9-14.material-ui.com/api/list/
   return (
     <div>
-      <Button
-        type='submit'
-        variant='contained'
-        className={clsx(classes.margin)}
-      >
-        Add Review
-      </Button>
+      <List>
+        <ListItem button={true}>
+          <ListItemText primary='Pizza' />
+        </ListItem>
+        <ListItem button={true}>
+          <ListItemText primary='Burger' />
+        </ListItem>
+      </List>
     </div>
   );
 };
@@ -88,14 +90,70 @@ const AppComponent = (props: AppProps | any) => {
 
 const mapStateToProps = (state: any): any => {
   return {
-    events: getHsmEvents(state),
+        events: getHsmEvents(state),
   };
 };
 
 const mapDispatchToProps = (dispatch: any) => {
   return bindActionCreators({
-    onLoadLogs: loadLog,
+        onLoadLogs: loadLog,
   }, dispatch);
 };
 
 export const App = connect(mapStateToProps, mapDispatchToProps)(AppComponent);
+
+/*
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import Divider from '@material-ui/core/Divider';
+import InboxIcon from '@material-ui/icons/Inbox';
+import DraftsIcon from '@material-ui/icons/Drafts';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    width: '100%',
+    maxWidth: 360,
+    backgroundColor: theme.palette.background.paper,
+  },
+}));
+
+function ListItemLink(props) {
+  return <ListItem button component="a" {...props} />;
+}
+
+export default function SimpleList() {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <List component="nav" aria-label="main mailbox folders">
+        <ListItem button>
+          <ListItemIcon>
+            <InboxIcon />
+          </ListItemIcon>
+          <ListItemText primary="Inbox" />
+        </ListItem>
+        <ListItem button>
+          <ListItemIcon>
+            <DraftsIcon />
+          </ListItemIcon>
+          <ListItemText primary="Drafts" />
+        </ListItem>
+      </List>
+      <Divider />
+      <List component="nav" aria-label="secondary mailbox folders">
+        <ListItem button>
+          <ListItemText primary="Trash" />
+        </ListItem>
+        <ListItemLink href="#simple-list">
+          <ListItemText primary="Spam" />
+        </ListItemLink>
+      </List>
+    </div>
+  );
+}
+*/
